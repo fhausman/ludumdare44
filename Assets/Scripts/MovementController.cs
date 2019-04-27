8a349d3020e32c01bbaf309bc30576483f10f42c
Filip Hausman <filip.hausman@gmail.com>
2019-04-27 13:41:37 +0200

    jumping and walking works

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,20 +6,26 @@ public class MovementController : MonoBehaviour
 {
     public float MaxSpeed = 10f;
     public float MoveForce = 365f;
-    public float JumpForce = 1000f;
+    public float JumpForce = 10f;
 
     private Rigidbody2D rb;
+    private Transform trans;
 
-    // Start is called before the first frame update
+    private bool facingRight = true;
+    private bool jump = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        trans = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
@@ -29,5 +35,44 @@ public class MovementController : MonoBehaviour
         rb.AddForce(Vector2.right * h * MoveForce);
         if (Mathf.Abs(rb.velocity.x) > MaxSpeed)
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MaxSpeed, rb.velocity.y);
+
+        if(ShouldFlip(h))
+        {
+            Flip();
+        }
+
+        if(jump)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        jump = false;
+        rb.AddForce(Vector2.up * JumpForce);
+    }
+
+    bool ShouldFlip(float input)
+    {
+        if (input > 0 && !facingRight)
+        {
+            return true;
+        }
+        else if (input < 0 && facingRight)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        var ls = trans.localScale;
+        ls.x *= -1;
+        trans.localScale = ls;
     }
 }
