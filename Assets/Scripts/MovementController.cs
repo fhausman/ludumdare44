3dd4474f429bbb277fb01d0643525c17c8b20617
Filip Hausman <filip.hausman@gmail.com>
2019-04-27 11:49:19 +0200

    initial movement script

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float MaxSpeed = 10f;
+    public float MoveForce = 365f;
+    public float JumpForce = 1000f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        var h = Input.GetAxis("Horizontal");
+
+        rb.AddForce(Vector2.right * h * MoveForce);
+        if (Mathf.Abs(rb.velocity.x) > MaxSpeed)
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * MaxSpeed, rb.velocity.y);
+    }
+}
